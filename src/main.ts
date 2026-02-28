06dc2fc104b8a4f91f83cbc1626f07de76de1084
Abdur Rahman <ansarians581@gmail.com>
2026-02-28 14:51:52 +0500

    Initial nest + typescript + mongoose + cloudinary + jwt setup

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import dotenv from 'dotenv'
+import { ValidationPipe } from '@nestjs/common';
+dotenv.config();
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //setting up cors
+  app.enableCors({
+    origin:process.env.FRONTEND_URL || "http://localhost:5173",
+    credentails: true,
+  })
+
+  //global validation pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    transform:true,
+  }))
+
+  //set common prefix
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3101;
+
+  await app.listen(port ?? 3101);
+  console.log(`Server listening on Port ${port}`);
+}
+bootstrap();
